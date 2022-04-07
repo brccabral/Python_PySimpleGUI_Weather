40902d263bae4c6bc3bc6ea1df4b353c2848e713
Bruno Cabral <cabral.brc@gmail.com>
2022-04-07 12:06:19 -0700

    initial layout

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
 import PySimpleGUI as sg
 
+image_col = sg.Column([[sg.Image("", key="-IMAGE-", background_color="white")]])
+info_col = sg.Column(
+    [
+        [
+            sg.Text(
+                "",
+                key="-LOCATION-",
+                font="Calibri 30",
+                background_color="red",
+                pad=0,
+                visible=False,
+            )
+        ],
+        [
+            sg.Text(
+                "",
+                key="-TIME-",
+                font="Calibri 16",
+                background_color="black",
+                text_color="white",
+                pad=0,
+                visible=False,
+            )
+        ],
+        [
+            sg.Text(
+                "",
+                key="-TIME-",
+                font="Calibri 16",
+                justification="center",
+                expand_x=False,
+                pad=(0, 10),
+                visible=False,
+                background_color="white",
+                text_color="black",
+            ),
+        ],
+    ]
+)
 
-layout = [[]]
+layout = [
+    [sg.Input(key="-INPUT-", expand_x=True), sg.Button("Enter", key="-ENTER-")],
+    [image_col, info_col],
+]
 
 window = sg.Window("Wheather", layout)
 
